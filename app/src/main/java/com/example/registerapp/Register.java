b00c97d3aec443e03e6dd58b75470efdfba13034
huunhat0602-hub <64886794+huunhat0602-hub@users.noreply.github.com>
2020-12-13 19:26:39 +0700

    huunhat

---
+++ app/src/main/java/com/example/registerapp/Register.java
@@ -0,0 +1,90 @@
+package com.example.registerapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+    EditText edtName, edtEmail, edtPassword, edtPhone;
+    Button btnRegister;
+    FirebaseAuth fAuth;
+    TextView tvLoginButton;
+    ProgressBar progressBarRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtPhone = findViewById(R.id.edtPhone);
+        btnRegister = findViewById(R.id.btnRegister);
+        progressBarRegister = findViewById(R.id.progressBarRegister);
+        tvLoginButton = findViewById(R.id.tvLoginButton);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    edtEmail.setError("Bạn chưa nhập email");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    edtPassword.setError("Email is Required");
+                    return;
+                }
+                if(password.length() < 6){
+                    edtPassword.setError("Mật khẩu phải trên 6 kí tự");
+                    return;
+                }
+                progressBarRegister.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "Tạo thành công tài khoản", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else{
+                            Toast.makeText(Register.this, "Error !"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            System.out.println( " loi "+ task.getException().getMessage());
+                            progressBarRegister.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+        tvLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+    }
+}
